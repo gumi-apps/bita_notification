65128f2bc7dc805ed863623ec5722d8a00718375
nurad <nuradhussen082@gmail.com>
2024-05-06 13:33:54 +0300

    project set up 101

---
+++ bin/bita_notification.dart
@@ -0,0 +1,5 @@
+import 'package:bita_notification/bita_notification.dart' as bita_notification;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bita_notification.calculate()}!');
+}
---
+++ test/bita_notification_test.dart
@@ -0,0 +1,8 @@
+import 'package:bita_notification/bita_notification.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
